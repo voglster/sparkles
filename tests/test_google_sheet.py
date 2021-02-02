25dd53b23b1130cbab9984ec1ecf23adc0133703
Jim Vogel <jim.m.vogel@gmail.com>
2021-02-02 14:25:19 -0600

    add test for filenotfound error

--- tests/test_google_sheet.py
+++ tests/test_google_sheet.py
@@ -1,1 +1,8 @@
+import pytest
+
 from sparkles import google_sheet
+
+
+def test_file_not_found_exception():
+    with pytest.raises(FileNotFoundError):
+        google_sheet.absolute_path_for("ZZZZZZZ.ZZZ")
